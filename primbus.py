126a10ca9999b61ef3b71a7612b3c101bd67dbae
Whoismeidc <54864427+Whoismeidc@users.noreply.github.com>
2021-04-23 01:39:40 -0600

    Update primbus.py 1
    Gave primbus.py a reason to be opened

--- primbus.py
+++ primbus.py
@@ -1,1 +1,32 @@
+# Course: CS 30
+# Quint: 4
+# Date created: 10/03/2021
+# Date last modified: 23/03/2021
+# Name: Mark Luu
+# Description: A demonstration of the application of
+# modules, complete with the creation of a map.
 
+from environments import *
+
+from resources import *
+
+from characters import *
+
+from enemies import *
+
+
+# Test characters and resources
+print(items['Shul'])
+
+# Test environments
+for map in organizedmap1:
+  print(f"{str(map)}")
+
+for map in organizedmap2:
+  print(f"{str(map)}")
+
+# Test resources
+print(weapon[1])
+
+# Test enemies
+print(goon)
