b2044e31294f7531cc747bbeed472270dab90fe2
Whoismeidc <54864427+Whoismeidc@users.noreply.github.com>
2021-04-23 01:35:56 -0600

    Made resources.py
    for use in characters and enemies

---
+++ resources.py
@@ -0,0 +1,13 @@
+# Resources are cash, and convert 1:1 to its equivalent
+# item. $10 goes to knives, sweaters, medkits, and any
+# boost. $100 gives a sword...
+
+resource = ['$10', '$100', '$1000', '$10000']
+
+weapon = ['knife', 'sword', 'greatsword', 'laser blade']
+
+armor = ['sweater', 'kevlar', 'armor']
+
+healing = ['medkit', 'potion', 'ambrosia']
+
+boost = ['flashbang', 'bomb', 'decoy']
