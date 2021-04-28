38058c50528478ba7ed2ed5a67cc1507999cc219
Whoismeidc <54864427+Whoismeidc@users.noreply.github.com>
2021-04-28 13:26:52 -0600

    Converted to Classes

--- characters.py
+++ characters.py
@@ -1,59 +1,20 @@
-from resources import *
+# Defense is the chance to cause a miss, from 0 to 100.
+# This is for flashbangs.
+# Armor decides how much less damage someone takes from
+# an attack, addition wise.
 
-# Dictionaries of character stats.
-charm = {
-    'Shul': 'Kensei',
-    'Kali': 'Data Wizard',
-    'Rey': 'Barrier Jacket'
-}
 
-# Character health is determined by stamina + 5.
-charstamina = {
-    'Shul': '2',
-    'Kali': '2',
-    'Rey': '4'
-}
+class Character:
+    def __init__(char, stamina, defense):
+        char.health = 5 + stamina
+        char.armor = 0
+        char.inventory = []
+        char.defense = 0
 
-# A holdover from the original tabletop that serves no
-# meaning here.
-identity = {
-    'Shul': 'Bryan Blodgett',
-    'Kali': 'Anna Addison',
-    'Rey': 'Nicko Scoops'
-}
+# The charm is the player's special ability.
 
-# Items can be found in resources dot py. Resource[0]
-# is cash, healing[0] is a medkit, and weapon[0] is
-# a knife.
-items = {
-    'Shul': [resource[0], healing[0]],
-    'Kali': [resource[0], healing[0]],
-    'Rey': [resource[0], weapon[0]]
-}
-# Shul's character sheet
 
-char0 = {
-  'Shul': {
-        'Charm': charm['Shul'],
-        'Stamina': charstamina['Shul'],
-        'Identity': identity['Shul']
-  }
-}
-
-# Kali's character sheet
-char1 = {
-    'Kali': {
-        'Charm': charm['Kali'],
-        'Stamina': charstamina['Kali'],
-        'Identity': identity['Kali']
-    }
-}
-
-# Rey's character sheet
-char2 = {
-  'Rey': {
-    'Charm': charm['Rey'],
-    'Stamina': charstamina['Rey'],
-    'Identity': identity['Rey']
-  }
-}
+class Player(Character):
+    def __init__(player, stamina, defense, charm):
+        super().__init__(stamina, defense)
+        player.charm = charm
