83a4fa9fcebe1fd6d8fc449d9e573dad888a1ed0
Whoismeidc <54864427+Whoismeidc@users.noreply.github.com>
2021-04-28 13:43:29 -0600

    Converted to classes, gave docstrings

--- resources.py
+++ resources.py
@@ -3,38 +3,45 @@
 
 class Explosive:
     def __init__(bomb, cost, dammod):
+        '''Attributes for bombs.'''
         bomb.dammod = dammod
         bomb.cost = cost
 
 
 class Flashbang(Explosive):
     def __init__(flash, cost, dammod, accmod):
+        '''Attributes for flashbangs.'''
         super().__init__(cost, dammod)
         flash.accmod = accmod
 
 
 class Decoy:
     def __init__(rope, escapechance):
+        '''Attributes for decoys.'''
         rope.escapechance = escapechance
 
 
 class Healing:
     def __init__(medkit, healing):
+        '''Attributes for healing items.'''
         medkit.healing = healing
 
 
 class Money:
     def __init__(stock, value):
+        '''Attributes for money items.'''
         stock.value = value
 
 
 class Armor:
     def __init__(vest, cost, armor):
+        '''Attributes for armor items.'''
         vest.cost = cost
         vest.armor = armor
 
 
 class Weapon:
     def __init__(knife, cost, damage):
+        '''Attributes for weapons.'''
         knife.cost = cost
         knife.damage = damage
